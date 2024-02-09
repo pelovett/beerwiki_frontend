caeed5ebc66b155cc3703535777b948680d502ab
pelovett@gmail.com <pelovett@gmail.com>
2024-02-08 18:37:03 -0800

    Adds basic page and favicon

--- app/beer/[id]/page.tsx
+++ app/beer/[id]/page.tsx
@@ -1,3 +1,31 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>;
+const BACKEND_SERVER = process.env.BACKEND_SERVER || "http://localhost:8888";
+const REVALIDATION_TIMEOUT_SEC = 60;
+
+export default async function Page({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const beerName = await getBeerInfo(id);
+  return <div>Beer Name: {beerName}</div>;
+}
+
+async function getBeerInfo(beer_id: string): Promise<string> {
+  let data;
+  try {
+    const res = await fetch(BACKEND_SERVER + `/beer/${beer_id}`, {
+      next: { revalidate: REVALIDATION_TIMEOUT_SEC },
+    });
+    data = await res.json();
+  } catch (err) {
+    console.error(err);
+    console.log(`Failed to fetch beer page: ${beer_id} err: ${err}`);
+  }
+
+  if (!data.message) {
+    console.error("No beer message returned from backend!");
+    throw new Error("No beer message returned from backend!");
+  }
+
+  return data.message;
 }
