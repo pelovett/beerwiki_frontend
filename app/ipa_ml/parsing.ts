49cd8c8f9619b32d1b5f9ff7900c63d38a585d7b
Benjamin Lain <chickenfrend@gmail.com>
2024-03-10 15:57:40 -0700

    Used prettier and also added new function and lookup table

--- app/ipa_ml/parsing.ts
+++ app/ipa_ml/parsing.ts
@@ -1,31 +1,53 @@
-
-const inBlockOpList = ['*'];
+const inBlockOpList = ["*"];
 
 interface opCache {
-    op: string,
-    cache: string,
+  op: string;
+  cache: string;
 }
 
+type operators = {
+  italic: "*";
+};
+
 interface formattedTextSection {
-    text: string,
-    isItalic: boolean,
-    isBold: boolean,
+  text: string;
+  lookupTable: Map<string, boolean>;
+}
+
+function getLookupTable(): Map<string, boolean> {
+  return new Map<string, boolean>(inBlockOpList.map((key) => [key, false]));
 }
 
 function parseBlock(rawText: string) {
-    let italicsFlag = false;
-    let textResult: formattedTextSection[] = [];
-    let curText = '';
-    for (let i = 0; i < rawText.length; i++) {
-        const curChar = rawText.at(i) ?? '';
-        if (inBlockOpList.includes(curChar)) {
-            italicsFlag = !italicsFlag;
-
-            textResult.push({text: curText, isItalic: italicsFlag, isBold: false});
-        }
-
-        curText += curChar;
+  let textResult: formattedTextSection[] = [];
+  for (let i = 0; i < rawText.length; i++) {
+    const curChar = rawText.at(i) ?? "";
+    if (inBlockOpList.includes(curChar)) {
+      const prevTextSection =
+        i > 0 ? textResult[textResult.length - 1] : undefined;
+      textResult.push(newTextSection(curChar, prevTextSection));
+    } else {
+      textResult[textResult.length - 1].text =
+        textResult[textResult.length - -1].text.concat(curChar);
     }
+  }
+  return textResult;
+}
 
-    return textResult;
+function newTextSection(
+  operator: string,
+  prevTextSection?: formattedTextSection,
+) {
+  let newTable = getLookupTable();
+  if (prevTextSection) {
+    newTable = prevTextSection.lookupTable;
+  }
+  newTable.set(operator, !newTable.get(operator));
+
+  const newTextSection: formattedTextSection = {
+    text: "",
+    lookupTable: newTable,
+  };
+
+  return newTextSection;
 }
