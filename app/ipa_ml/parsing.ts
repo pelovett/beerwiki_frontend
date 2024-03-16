90ec95a3f0368ff9150ae36e7aa4a1e65be47521
Benjamin Lain <chickenfrend@gmail.com>
2024-03-16 14:36:38 -0700

    Added tracking of operator locations

--- app/ipa_ml/parsing.ts
+++ app/ipa_ml/parsing.ts
@@ -19,17 +19,26 @@ export function getLookupTable(): Map<string, boolean> {
 }
 
 export function parseBlock(rawText: string) {
-  let textResult: formattedTextSection[] = [];
-  textResult.push(newTextSection());
+  let textResult: Map<number, formattedTextSection> = new Map<number, formattedTextSection>;
+  let opLastSeen: Map<string, number> = new Map<string, number>();
+  let prevIndex = 0;
+  textResult.set(0, newTextSection());
   for (let i = 0; i < rawText.length; i++) {
     const curChar = rawText.at(i) ?? "";
     if (inBlockOpList.includes(curChar)) {
       const prevTextSection =
-        i > 0 ? textResult[textResult.length - 1] : undefined;
-      textResult.push(newTextSection(curChar, prevTextSection));
+        i > 0 ? textResult.get(prevIndex) : undefined;
+      textResult.set(i, newTextSection(curChar, prevTextSection));
+      prevIndex = i;
     } else {
-      textResult[textResult.length - 1].text =
-        textResult[textResult.length - 1].text.concat(curChar);
+        // This all should maybe be extracted
+        // Also I don't like it -Ben
+      let currentText = textResult.get(prevIndex);
+      let newText: formattedTextSection = {
+         text: currentText ? currentText.text.concat(curChar) : '',
+         lookupTable: currentText?.lookupTable ?? getLookupTable(),
+      }
+      textResult.set(prevIndex, newText);
     }
   }
   return textResult;
