ceb23bda8a27a66982941842520be69f778b9d97
Benjamin Lain <chickenfrend@gmail.com>
2024-05-03 19:47:12 -0700

    Added one little function to output italic or bold or bold italic jsx lol

--- app/ipa_ml/parsing.ts
+++ app/ipa_ml/parsing.ts
@@ -1,18 +1,18 @@
 const inBlockOpList = ["*", "`"];
 
-interface opCache {
+type opCache = {
   op: string;
   cache: string;
-}
+};
 
 type operators = {
   italic: "*";
 };
 
-export interface formattedTextSection {
+export type formattedTextSection = {
   text: string;
   lookupTable: Map<string, boolean>;
-}
+};
 
 export function getLookupTable(): Map<string, boolean> {
   return new Map<string, boolean>(inBlockOpList.map((key) => [key, false]));
@@ -37,8 +37,6 @@ export function parseBlock(rawText: string) {
       textResult.set(i, newTextSection(curChar, prevTextSection));
       prevIndex = i;
     } else {
-      // This all should maybe be extracted
-      // Also I don't like it -Ben
       let currentText = textResult.get(prevIndex);
       let newText: formattedTextSection = {
         text: currentText ? currentText.text.concat(curChar) : "",
@@ -56,32 +54,39 @@ function fixUnmatchedSections(
   opCount: Map<string, number>,
   textResult: Map<number, formattedTextSection>,
 ) {
-    opCount.forEach((count, operator) => {
-        if (count % 2 === 1) {
-            fixUnmatchedOp(operator, opLastSeen.get(operator) ?? 0, textResult);
-        }
-    });
+  opCount.forEach((count, operator) => {
+    if (count % 2 === 1) {
+      fixUnmatchedOp(operator, opLastSeen.get(operator) ?? 0, textResult);
+    }
+  });
 }
 
 // Inserts operator into text and removes op flag from all following text sections.
-function fixUnmatchedOp(operator: string, opPos: number, textResult: Map<number, formattedTextSection>) {
-    const prevText = textResult.get(opPos)?.text ?? '';
-    const newText = operator + prevText;
-    const newTextSection: formattedTextSection = {
-       text: newText,
-       lookupTable: textResult.get(opPos)?.lookupTable ?? getLookupTable(),
+function fixUnmatchedOp(
+  operator: string,
+  opPos: number,
+  textResult: Map<number, formattedTextSection>,
+) {
+  const prevText = textResult.get(opPos)?.text ?? "";
+  const newText = operator + prevText;
+  const newTextSection: formattedTextSection = {
+    text: newText,
+    lookupTable: textResult.get(opPos)?.lookupTable ?? getLookupTable(),
+  };
+  textResult.set(opPos, newTextSection);
+
+  textResult.forEach((textSection, position) => {
+    if (position >= opPos) {
+      setOpFalseInTable(operator, textSection.lookupTable);
     }
-    textResult.set(opPos, newTextSection);
-    
-    textResult.forEach((textSection, position) => {
-        if (position >= opPos) {
-            setOpFalseInTable(operator, textSection.lookupTable);
-        }
-    });
+  });
 }
 
-function setOpFalseInTable(operator: string, lookupTable: Map<string, boolean>) {
-    lookupTable.set(operator, false)
+function setOpFalseInTable(
+  operator: string,
+  lookupTable: Map<string, boolean>,
+) {
+  lookupTable.set(operator, false);
 }
 
 function newTextSection(
