2f588a84a918a0db9862f2bf0a2c84504734ccc5
Benjamin Lain <chickenfrend@gmail.com>
2024-05-27 16:56:45 -0700

    editing works now

---
+++ app/new_page/ipa_ml_input.tsx
@@ -0,0 +1,20 @@
+type IpaMlInputProps = {
+  inputText: string;
+  setInputText: (payload: string) => void;
+};
+
+export function IpaMlInput({ inputText, setInputText }: IpaMlInputProps) {
+  const handleInputChange = (event: { target: { value: string } }) => {
+    setInputText(event.target.value);
+  };
+
+  return (
+    <textarea
+      name="IPAML Input"
+      autoComplete="off"
+      className="block h-full w-6/12 rounded-md border-0 py-1.5 pl-3.5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 mx-5"
+      value={inputText}
+      onChange={handleInputChange}
+    />
+  );
+}
