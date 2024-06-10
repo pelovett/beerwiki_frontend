99da681c03422112d651c3a2ccf60fcbb4519f4c
Evan <evankender503@gmail.com>
2024-06-09 17:49:59 -0700

    handle cookie in header not body

--- app/components/verify_user.tsx
+++ app/components/verify_user.tsx
@@ -1,26 +1,23 @@
 import { cookies } from "next/headers";
-import { NEXT_PUBLIC_BACKEND_SERVER  } from "../network/util";
+import { NEXT_PUBLIC_BACKEND_SERVER } from "../network/util";
 
 export default async function VerifyUser(): Promise<Boolean> {
   const authCookie = cookies().get("login_cookie");
-  if (authCookie?.value != "") {
-    console.log(NEXT_PUBLIC_BACKEND_SERVER )
-    const response = await fetch(NEXT_PUBLIC_BACKEND_SERVER  + "/user/verify", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ tokenString: authCookie?.value }),
-    });
-
-    console.log(response);
-    if (response.ok) {
-      console.log("good");
-      return true;
-    } else {
-      console.error(response);
-      console.log;
-      return false;
-    }
+  if (!authCookie || !authCookie?.value) {
+    return false;
   }
-  console.log("Invalid tokenString");
-  return false;
+
+  console.log(NEXT_PUBLIC_BACKEND_SERVER);
+  const response = await fetch(NEXT_PUBLIC_BACKEND_SERVER + "/user/verify", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Cookie: authCookie.value },
+    body: JSON.stringify({ tokenString: authCookie?.value }),
+  });
+
+  console.log(response);
+  if (response.ok) {
+    return true;
+  } else {
+    return false;
+  }
 }
