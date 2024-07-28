ad67e03dc965aa78f590e89e38cc4ce90a07daf8
Benjamin Lain <chickenfrend@gmail.com>
2024-07-28 16:20:05 -0700

    Save button now takes you to the main beer page

--- app/beer/edit/[name]/page.tsx
+++ app/beer/edit/[name]/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useEffect } from "react";
-import { formatAndRenderText } from "../../ipa_ml/ml_rendering";
+import { formatAndRenderText } from "../../../ipa_ml/ml_rendering";
 import { IpaMlInput } from "../ipa_ml_input";
 import { useState } from "react";
-import { getBeerIPAML, setBeerIPAML } from "../../api_calls/beer_calls";
+import { getBeerIPAML, setBeerIPAML } from "../../../api_calls/beer_calls";
 
 const REVALIDATION_TIMEOUT_SEC = 60;
 
@@ -28,21 +28,17 @@ export default function Page({
 
   function saveBeerDescription() {
     setBeerIPAML(name, inputText);
+    window.location.href = `/beer/${name}`;
   }
 
   const textJsx = formatAndRenderText(inputText);
-  console.log("test");
-  console.log(textJsx);
   return (
     <div className="flex flex-col w-full h-full my-6">
       <div className="flex flex-row self-center w-full h-full my-6">
         <div className="w-2/4 mx-5">
           <IpaMlInput inputText={inputText} setInputText={setInputText} />
         </div>
-        <div className="max-w-[50%] w-2/4 mx-5">
-          <h1>test!</h1>
-          {textJsx}
-        </div>
+        <div className="max-w-[50%] w-2/4 mx-5">{textJsx}</div>
       </div>
       <div className="flex flex-row-reverse w-4/5 mx-8">
         <button
