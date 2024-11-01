4e09a053fe1a4a15f11242a035ff666585180f20
Benjamin Lain <chickenfrend@gmail.com>
2024-10-31 22:03:08 -0700

    Started creating mobile version of edit page

--- app/beer/edit/[name]/page.tsx
+++ app/beer/edit/[name]/page.tsx
@@ -7,6 +7,7 @@ import SideBar from "@/app/components/side_bar";
 import { formatAndRenderText } from "../../../ipa_ml/ml_rendering";
 import { IpaMlInput } from "../ipa_ml_input";
 import { getBeerIPAML, setBeerIPAML } from "../../../api_calls/beer_calls";
+import IPAMLEditor from "../../../components/ipa_ml_editor";
 
 const REVALIDATION_TIMEOUT_SEC = 60;
 
@@ -21,7 +22,7 @@ export default function Page({
     async function getDescriptionFromAPI() {
       const beerDescription = await getBeerIPAML(
         name,
-        REVALIDATION_TIMEOUT_SEC
+        REVALIDATION_TIMEOUT_SEC,
       );
       setInputText(beerDescription);
     }
@@ -37,14 +38,33 @@ export default function Page({
     }
   }
 
+  const editor = IPAMLEditor();
+
   const textJsx = formatAndRenderText(inputText);
   return (
     <>
       <TopBar />
+      <nav className="flex sm:hidden justify-between items-center py-2 mx-3">
+        <button
+          type="submit"
+          className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+          onClick={saveBeerDescription}
+        >
+          Save
+        </button>
+        <label className="relative inline-flex cursor-pointer items-center">
+          <input type="checkbox" value="" defaultChecked className="peer sr-only" />
+          <div className="peer flex h-8 items-center gap-5 rounded-full bg-orange-600 px-7 after:absolute after:left-1 after: after:h-6 after:w-16 after:rounded-full after:bg-white/40 after:transition-all after:content-[''] peer-checked:bg-stone-600 peer-checked:after:translate-x-full peer-focus:outline-none dark:border-slate-600 dark:bg-slate-700 text-sm text-white">
+            <span>Edit</span>
+            <span>Preview</span>
+          </div>
+        </label>{" "}
+      </nav>
       <div className="flex flex-col w-full h-full my-3 sm:my-6">
         <div className="flex flex-row self-center w-full h-full my-6">
           <SideBar />
           <div className="w-2/4 mx-5">
+            {editor}
             <IpaMlInput
               inputText={inputText}
               setInputText={setInputText}
@@ -53,7 +73,7 @@ export default function Page({
           </div>
           <div className="max-w-[50%] w-2/4 mx-5">{textJsx}</div>
         </div>
-        <div className="flex flex-row-reverse w-4/5 mx-8">
+        <div className="hidden sm:flex flex-row-reverse w-4/5 mx-8">
           <button
             type="submit"
             className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
--- app/components/ipa_ml_editor.tsx
+++ app/components/ipa_ml_editor.tsx
@@ -1,3 +1,4 @@
+import {ReactElement} from "react";
 
 export default function IPAMLEditor() {
   const bBar = EditorButtonBar();
@@ -11,8 +12,8 @@ export default function IPAMLEditor() {
 }
 
 function EditorButtonBar() {
-  const boldButton = ButtonChip('B');
-  const italicsButton = ButtonChip('I');
+  const boldButton = ButtonChip({bSymbol: 'B', emphasis: 'font-bold'});
+  const italicsButton = ButtonChip({bSymbol: 'i', emphasis: 'font-style: italic'});
   return (
     <div className = "flex flex-row w-full border-solid border-2 border-gray-300 rounded e-md h-[2rem]">
       {boldButton}
@@ -21,11 +22,18 @@ function EditorButtonBar() {
   )
 }
 
-function ButtonChip(bSymbol: string) {
+function ButtonChip({
+    bSymbol, 
+    emphasis = "", 
+}: {
+    bSymbol: string;
+    emphasis: string;
+}): ReactElement {
     return (
         <button
           className={
-            "flex justify-center items-center w-[2rem] h-full hover:bg-stone-300 border-r-2"
+            "flex justify-center items-center w-[2rem] h-full hover:bg-stone-300 border-r-2 "
+            + emphasis
           }
         >
           {bSymbol}
