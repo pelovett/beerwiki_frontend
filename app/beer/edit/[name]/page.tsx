07315318c1e130d3aaf4ae03667b7f12f7e204e2
Benjamin Lain <chickenfrend@gmail.com>
2024-11-10 15:19:49 -0800

    Toggling works now. Looks bad but works

--- app/beer/edit/[name]/page.tsx
+++ app/beer/edit/[name]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, ReactElement } from "react";
 
 import TopBar from "@/app/components/top_bar";
 import SideBar from "@/app/components/side_bar";
@@ -40,28 +40,77 @@ export default function Page({
 
   const editor = IPAMLEditor();
 
+  const [checked, setChecked] = useState(false);
+
+  const editTopBar = (
+    <nav className="flex sm:hidden justify-between items-center py-2 mx-3">
+      <button
+        type="submit"
+        className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+        onClick={saveBeerDescription}
+      >
+        Save
+      </button>
+      <CheckHeader checked={checked} setChecked={setChecked} />
+    </nav>
+  );
+
+  function CheckHeader({
+    checked,
+    setChecked,
+  }: {
+    checked: boolean;
+    setChecked: (checked: boolean) => void;
+  }): ReactElement {
+    return (
+      <label className="relative inline-flex cursor-pointer items-center">
+        <input
+          type="checkbox"
+          value=""
+          checked={checked}
+          onChange={() => {
+            setChecked(!checked);
+          }}
+          className="peer sr-only"
+        />
+        <div
+          className={
+            "peer flex h-10 items-center gap-5 " +
+            "rounded-full bg-blue-200 px-7 after:absolute " +
+            "after:left-1 after: after:h-8 after:w-16 after:rounded-full " +
+            "after:bg-blue-500 after:transition-all after:content-[''] " +
+            "peer-checked:bg-blue-200 peer-checked:after:translate-x-full " +
+            "peer-focus:outline-none text-sm text-white"
+          }
+        >
+          <span className="z-10 select-none">Edit</span>
+          <span className="z-10 select-none">Preview</span>
+        </div>
+      </label>
+    );
+  }
+
   const textJsx = formatAndRenderText(inputText);
   return (
     <>
       <TopBar />
-      <nav className="flex sm:hidden justify-between items-center py-2 mx-3">
-        <button
-          type="submit"
-          className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-          onClick={saveBeerDescription}
-        >
-          Save
-        </button>
-        <label className="relative inline-flex cursor-pointer items-center">
-          <input type="checkbox" value="" defaultChecked className="peer sr-only" />
-          <div className="peer flex h-8 items-center gap-5 rounded-full bg-orange-600 px-7 after:absolute after:left-1 after: after:h-6 after:w-16 after:rounded-full after:bg-white/40 after:transition-all after:content-[''] peer-checked:bg-stone-600 peer-checked:after:translate-x-full peer-focus:outline-none dark:border-slate-600 dark:bg-slate-700 text-sm text-white">
-            <span>Edit</span>
-            <span>Preview</span>
-          </div>
-        </label>{" "}
-      </nav>
+      {editTopBar}
       <div className="flex flex-col w-full h-full my-3 sm:my-6">
-        <div className="flex flex-row self-center w-full h-full my-6">
+        <div className="flex sm:hidden flex-row self-center w-full h-full my-6">
+          {checked ? (
+            <div className="flex w-full mx-4">{textJsx}</div>
+          ) : (
+            <div className="flex flex-col w-full mx-4">
+              {editor}
+              <IpaMlInput
+                inputText={inputText}
+                setInputText={setInputText}
+                disabled={loading}
+              />
+            </div>
+          )}
+        </div>
+        <div className="hidden sm:flex flex-row self-center w-full h-full my-6">
           <SideBar />
           <div className="w-2/4 mx-5">
             {editor}
@@ -73,10 +122,10 @@ export default function Page({
           </div>
           <div className="max-w-[50%] w-2/4 mx-5">{textJsx}</div>
         </div>
-        <div className="hidden sm:flex flex-row-reverse w-4/5 mx-8">
+        <div className="hidden sm:flex flex-row-reverse w-4/5">
           <button
             type="submit"
-            className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+            className="p-2 h-8 bg-blue-500 text-white rounded-md hover:bg-blue-600"
             onClick={saveBeerDescription}
           >
             Save
